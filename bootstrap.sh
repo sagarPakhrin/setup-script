681ebbc6d037e9c4a263e3b0b87e7ef696e827ee
Kevin Hughes <kevinhughes27@gmail.com>
2017-12-18 13:13:23 -0500

    install setuptools

--- bootstrap.sh
+++ bootstrap.sh
@@ -151,6 +151,7 @@ read -p "Install python (Y/y)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo apt-get install python3-pip
     pip3 install --upgrade pip
+    sudo pip3 install setuptools
 fi
 
 
